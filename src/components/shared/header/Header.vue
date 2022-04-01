66fd45cf4d9c9c6a2286e28851e65d19d48e2521
raffa <raffaeleloi121@gmail.com>
2022-04-01 13:34:06 -0300

    adding vue router

---
+++ src/components/shared/header/Header.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="header">
+  <h3 class="header-title">Star wars with Vue</h3>
+  <nav>
+    <ul class="menu-list">
+      <li class="menu-list__item">Home</li>
+      <li class="menu-list__item">Motivation</li>
+      <li class="menu-list__item">About</li>
+    </ul>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.header {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0px 3px 3px 1px gray;
+  padding: 10px;
+}
+
+.header-title {
+ margin-left: 1rem;
+}
+
+.header-title:hover {
+ cursor: pointer;
+}
+
+.menu-list {
+  display: flex;
+}
+
+.menu-list__item {
+  list-style: none;
+  padding: 5px;
+  margin-right: 1rem;
+}
+
+.menu-list__item {
+  cursor: pointer;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import { routes } from './routes.js';
 
-createApp(App).mount('#app')
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes
+});
+
+const app = Vue.createApp({});
+app.use(router);
+app.mount('#app');
